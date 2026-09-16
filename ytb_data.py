4cd8472856bf9f59fb203c264cc3ad1056f796a8
Ishak Laaouafi <laaouafi6@gmail.com>
2026-09-16 11:06:59 +0100

    j'ai creé le fichier json

---
+++ ytb_data.py
@@ -0,0 +1,61 @@
+from googleapiclient.discovery import build
+import json
+
+
+api_key = "AIzaSyCbRCbf6TSQzZWhUXSMtMxjEt_9RwEmRJA"
+youtube = build('youtube','v3',developerKey = api_key)
+
+channel_id = 'UC0WP5P-ufpRfjbNrmOWwLBQ'
+
+channel_response = youtube.channels().list(
+    id=channel_id,
+    part='contentDetails'
+).execute()
+
+uploads_playlist_id = channel_response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+video_ids = []
+next_page_token = None
+
+while True:
+    playlist_response = youtube.playlistItems().list(
+        playlistId=uploads_playlist_id,
+        part='contentDetails',
+        maxResults=50,
+        pageToken=next_page_token
+    ).execute()
+
+    for item in playlist_response['items']:
+        video_ids.append(item['contentDetails']['videoId'])
+
+    next_page_token = playlist_response.get('nextPageToken')
+    if not next_page_token:
+        break
+    
+
+all_video_data = []
+
+for i in range(0, len(video_ids), 50):
+    batch = video_ids[i:i+50]
+    response = youtube.videos().list(
+        id=','.join(batch),
+        part='snippet,contentDetails,statistics'
+    ).execute()
+
+    for item in response['items']:
+        video_data = {
+            'video_id': item['id'],
+            'title': item['snippet']['title'],
+            'published_at': item['snippet']['publishedAt'],
+            'duration': item['contentDetails']['duration'],
+            'view_count': item['statistics'].get('viewCount', 0),
+            'like_count': item['statistics'].get('likeCount', 0),
+            'comment_count': item['statistics'].get('commentCount', 0),
+        }
+        all_video_data.append(video_data)
+
+
+     
+with open("ytDATA.json", 'w', encoding='utf-8') as f:
+    json.dump(all_video_data, f, indent=2, ensure_ascii=False)
+
+print(f"Données sauvegardées dans ytDATA.json")
